971357cddb17917e88fc6c194bf83e44e0804ae9
maria-n14 <marianigos17@mittymonarch.com>
2016-08-05 14:20:58 -0700

    stuff

---
+++ Assets/Inventory/Ivn_scripts/Inventory.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Inventory : MonoBehaviour {
+
+    private RectTransform inventoryRect;
+    private float inventoryWidth, inventoryHeight;
+    
+    public int slots;
+    public int rows;
+    public float slotPaddingLeft, slotPaddingTop;
+    public float slotSize;
+    public GameObject slotPrefab;
+
+    private List<GameObject> allSlots;
+
+	// Use this for initialization
+	void Start () {
+        CreateLayout();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    private void CreateLayout()
+    {
+        allSlots = new List<GameObject>();
+
+        inventoryWidth = ((slots / rows) * (slotSize + slotPaddingLeft) + slotPaddingLeft);
+
+        inventoryHeight = rows * (slotSize + slotPaddingTop) + slotPaddingTop;
+
+        inventoryRect = GetComponent<RectTransform>();
+
+        inventoryRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, inventoryWidth);
+
+        inventoryRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, inventoryWidth);
+    }
+}
